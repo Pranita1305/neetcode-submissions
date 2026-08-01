1443578260a28713e9e280c2ee02241167bdeaf5
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2026-08-01 14:35:13 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> memo(m+1,vector<int>(n+1,0));
+
+        memo[m-1][n-1]=1;
+
+        for(int i=0;i<m;i++){
+            memo[i][n-1]=1;
+        }
+        for(int i=0;i<n;i++){
+            memo[m-1][i]=1;
+        }
+
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                memo[i][j]=memo[i][j+1]+memo[i+1][j];
+            }
+        }
+
+        return memo[0][0];
+    }
+};
